03df24a6ade84d9ac21b727c002814f27c461cb6
ZahideImanova <ld76qe8m1@code.edu.az>
2024-01-23 14:42:42 +0400

    byby

---
+++ script.js
@@ -0,0 +1,14 @@
+let cart=document.querySelector(".cart")
+let name=document.querySelector("#name")
+let price=document.querySelector("#price")
+let form=document.querySelector("form")
+
+function GetAllData(){
+    fetch ("url")
+    .then(res=>res.json)
+    .then(data=>{
+        innerHTML+=`
+        `
+    })
+    
+}
